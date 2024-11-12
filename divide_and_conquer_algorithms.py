1607c4b000f797cee9023a3a6e77b1e2eadb9f7e
Ferdinand Xiong <ga94cug@mytum.de>
2024-11-12 15:43:42 +0100

    Correct Implementation of Maximum Sum

--- divide_and_conquer_algorithms.py
+++ divide_and_conquer_algorithms.py
@@ -1,36 +1,36 @@
 from collections import namedtuple
 
-# Define the named tuple for structured access
-SubarrayResult = namedtuple('SubarrayResult', ['max_sum', 'start', 'end'])
-
 # algorithm returns the value of the maximal sum, the start and end index
 def max_sum_array(array, start, end):
     # for arrays of length 1, we return the single element list and its indices
     if start == end:
-        return SubarrayResult(array[start], start, end)  
-    # for bigger arrays, we need to divide    
+        return array[start]
+    # for bigger arrays, we need to DIVIDE    
     else:
         mid = (start + end)//2
         maxSumLeft = max_sum_array(array, start, mid)
         maxSumRight = max_sum_array(array, mid + 1, end)
 
-        # in the conquer step, we return the bigger sum if it is bigger than the arraysum 
-        # from the start indice of the left array to the end indice to the right array. 
-        # Instead summing from the left to the right end of the array,
-        # we should calculate the value with the formular:
-        # maxSumLeft.max_sum + sum(maxSumLeft.end + 1 to maxSumRight.start) + maxSumRight.max_sum
-        # Then we take the max between these 3 elements
-        valueBetweenLeftAndRight = 0
-        for i in range(maxSumLeft.end + 1, maxSumRight.start):
-            valueBetweenLeftAndRight += array[i]
+        # in the CONQUER step, we return the biggest sum between left, right and crossing sum
+        # the crossing sum at least has the last element of the left array and the first element of
+        # the right array. From these two elements, we need to iterate to the start of the left array
+        # and the end of the right array and find the max value for them and sum  them up to find the
+        # maximal crossing value
         
-        if(maxSumLeft.max_sum + valueBetweenLeftAndRight <= 0):
-            if (maxSumLeft.max_sum < maxSumRight.max_sum):
-                return maxSumRight
-            else:
-                return maxSumLeft
-        elif(maxSumRight.max_sum + valueBetweenLeftAndRight <= 0):
-            return maxSumLeft
-        else:
-            return SubarrayResult(maxSumLeft.max_sum + valueBetweenLeftAndRight + maxSumRight.max_sum,
-                                  maxSumLeft.start, maxSumRight.end)
+        maxCrossingLeft = array[mid]
+        crossingValue = 0
+        for i in range(mid, start, -1):
+            crossingValue += array[i]
+            if(crossingValue>=maxCrossingLeft):
+                maxCrossingLeft = crossingValue
+                
+        maxCrossingRight = array[mid +1]
+        crossingValue = 0
+        for i in range(mid +1, end):
+            crossingValue += array[i]
+            if(crossingValue>=maxCrossingRight):
+                maxCrossingRight = crossingValue
+                
+        maxCrossingSum = maxCrossingLeft + maxCrossingRight
+        
+        return max(maxSumLeft, maxSumRight, maxCrossingSum)
